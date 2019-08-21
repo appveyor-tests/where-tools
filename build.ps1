883d331c1337c5639d4157d181adb2df72de6904
Alena Hrynkevich <alena@appveyor.com>
2019-08-20 18:23:42 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 get-path
-$whereCommands = @("cmd /c where git", "cmd /c where svn")
+$whereCommands = @("cmd /c where git",  "cmd /c where bla", "cmd /c where svn")
 foreach ($Command in $whereCommands) {Invoke-Expression $Command ; if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }}
 
 
